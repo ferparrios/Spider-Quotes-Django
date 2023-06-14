2e1be89ff69797b003d7f765a3ff34a31e888f3e
Fernando Paredes Rios <fprios112@gmail.com>
2023-06-14 15:11:56 -0500

    adding security to the api

--- quotes/urls.py
+++ quotes/urls.py
@@ -8,5 +8,6 @@ router.register(r'quote', views.QuoteView, 'quotes')
 
 urlpatterns = [
     path("api/v1/", include(router.urls) ),
-    path("docs/", include_docs_urls(title="Spider-Quotes API"))
+    path("docs/", include_docs_urls(title="Spider-Quotes API")),
+    # path("", views.getData)
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,8 +1,12 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializer import QuoteSerializer
 from .models import Quote
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class QuoteView(viewsets.ModelViewSet):
+
+class QuoteView(viewsets.ReadOnlyModelViewSet):
     serializer_class = QuoteSerializer
     queryset = Quote.objects.all()
